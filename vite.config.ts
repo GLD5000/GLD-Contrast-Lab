2a0b13f19f1eac9ed08ab33461c0a8494046c052
GLD5000 <garethlouisdevlin@gmail.com>
2023-02-08 16:52:09 +0000

    Added vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'import.meta.vitest': 'undefined', // For inline testing
+  },
+  test: {
+    includeSource: ['src/**/*.{js,ts}'],
+    coverage: {
+      reporter: ['text', 'html'],
+    },
+  },
   plugins: [react()],
   build: {
     sourcemap: true,
