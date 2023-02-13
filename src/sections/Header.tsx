3dc01c6099e14956e9c6dbd3aca0aa00571815f4
GLD5000 <garethlouisdevlin@gmail.com>
2023-02-13 23:08:16 +0000

    Added button type

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -3,7 +3,7 @@ import GldSvg from '../icons/GldSvg';
 export default function Header({ title }: { title: string }) {
   return (
     <>
-      <button className="flex w-screen flex-col  bg-neutral-700">
+      <button type="button" className="flex w-screen flex-col  bg-neutral-700">
         <h1 className=" m-6 whitespace-pre text-blue-400">Hi, I am Gareth...</h1>
         <h2 className="m-4 whitespace-pre  text-blue-400">Welcome to my portfolio! to see more click here...</h2>
       </button>
