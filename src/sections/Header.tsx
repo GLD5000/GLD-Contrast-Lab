d7d4b96bfdeb217b850542cdf8ceb18f61c582b8
GLD5000 <garethlouisdevlin@gmail.com>
2023-02-13 23:07:43 +0000

    Updated header

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -2,11 +2,11 @@ import GldSvg from '../icons/GldSvg';
 
 export default function Header({ title }: { title: string }) {
   return (
-    <section className="flex w-screen flex-col">
-      <h1 className=" self-center whitespace-pre bg-neutral-800">Hello, my name is Gareth AKA GLD5000! </h1>
-      <h2 className="self-center whitespace-pre bg-neutral-800">
-        This is one of my portfolio projects- to see more click here!
-      </h2>
+    <>
+      <button className="flex w-screen flex-col  bg-neutral-700">
+        <h1 className=" m-6 whitespace-pre text-blue-400">Hi, I am Gareth...</h1>
+        <h2 className="m-4 whitespace-pre  text-blue-400">Welcome to my portfolio! to see more click here...</h2>
+      </button>
 
       <header className="sticky top-0 left-0 right-0 z-[999] grid h-fit w-screen flex-grow-0 grid-cols-frAutoFr content-center bg-neutral-800">
         <nav className=" col-start-2 flex w-body min-w-body max-w-body flex-wrap items-center justify-between align-middle  ">
@@ -17,6 +17,6 @@ export default function Header({ title }: { title: string }) {
           <div className="relative flex h-16 flex-wrap items-center justify-center gap-4 py-2" />
         </nav>
       </header>
-    </section>
+    </>
   );
 }
