e2e7139db7da0b4b2ceae9beb138191ae77b858d
GLD5000 <garethlouisdevlin@gmail.com>
2023-02-13 23:16:15 +0000

    Extracted portfolio component

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,13 +1,10 @@
 import GldSvg from '../icons/GldSvg';
+import Portfolio from './header/Portfolio';
 
 export default function Header({ title }: { title: string }) {
   return (
     <>
-      <button type="button" className="flex w-screen flex-col  bg-neutral-700">
-        <h1 className=" m-6 whitespace-pre text-blue-400">Hi, I am Gareth...</h1>
-        <h2 className="m-4 whitespace-pre  text-blue-400">Welcome to my portfolio! to see more click here...</h2>
-      </button>
-
+      <Portfolio />
       <header className="sticky top-0 left-0 right-0 z-[999] grid h-fit w-screen flex-grow-0 grid-cols-frAutoFr content-center bg-neutral-800">
         <nav className=" col-start-2 flex w-body min-w-body max-w-body flex-wrap items-center justify-between align-middle  ">
           <div className="flex h-16 flex-wrap items-center gap-4 p-2">
---
+++ src/sections/header/Portfolio.tsx
@@ -0,0 +1,8 @@
+export default function Portfolio() {
+  return (
+    <button type="button" className="flex w-screen flex-col  bg-neutral-700">
+      <h1 className=" m-6 whitespace-pre text-blue-400">Hi, I am Gareth...</h1>
+      <h2 className="m-4 whitespace-pre  text-blue-400">Welcome to my portfolio! to see more click here...</h2>
+    </button>
+  );
+}
