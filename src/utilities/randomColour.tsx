d1fd7ed53272ca72ba9648c4b4a26e4b6553b125
GLD5000 <garethlouisdevlin@gmail.com>
2023-02-17 18:10:18 +0000

    testing husky

--- src/utilities/randomColour.tsx
+++ src/utilities/randomColour.tsx
@@ -135,7 +135,7 @@ if (import.meta.vitest) {
     const output = '#0055ff';
     describe(`#${testFunc}`, () => {
       it(`works for ${input}`, () => {
-        expect((randomColour[testFunc](input))).toStrictEqual(output);
+        expect(((randomColour[testFunc](input)))).toStrictEqual(output);
       });
     });
   })();
@@ -146,7 +146,7 @@ if (import.meta.vitest) {
     const output = '#5500ff';
     describe(`#${testFunc}`, () => {
       it(`works for ${input}`, () => {
-        expect((randomColour[testFunc](input))).toStrictEqual(output);
+        expect(randomColour[testFunc](input)).toStrictEqual(output);
       });
     });
   })();
