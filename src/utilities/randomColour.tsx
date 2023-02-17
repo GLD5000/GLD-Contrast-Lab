547ee0b730fb224ad8b7f6a4c6e1152ea267a6cd
GLD5000 <garethlouisdevlin@gmail.com>
2023-02-17 17:59:06 +0000

    Lint staged test

--- src/utilities/randomColour.tsx
+++ src/utilities/randomColour.tsx
@@ -146,7 +146,7 @@ if (import.meta.vitest) {
     const output = '#5500ff';
     describe(`#${testFunc}`, () => {
       it(`works for ${input}`, () => {
-        expect(randomColour[testFunc](input)).toStrictEqual(output);
+        expect((randomColour[testFunc](input))).toStrictEqual(output);
       });
     });
   })();
