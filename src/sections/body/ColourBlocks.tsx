e1299f14779ceb63e444f6030aae6d0ac5449bee
GLD5000 <garethlouisdevlin@gmail.com>
2023-02-22 19:06:40 +0000

    Extracted block row function

--- src/sections/body/ColourBlocks.tsx
+++ src/sections/body/ColourBlocks.tsx
@@ -3,33 +3,44 @@ import ColourBlock from './ColourBlock';
 import autoTextColourFromHex from '../../utilities/colour/autoTextColour';
 import { getContrastRatioFromHex, contrast } from '../../utilities/colour/contrastRatio';
 
+function getBlockRow(
+  backgroundColour: string,
+  index: number,
+  array: string[],
+  clicked: boolean,
+  setClicked: Dispatch<SetStateAction<boolean>>,
+) {
+  const keyA = `${backgroundColour}${index}`;
+  const rowArray = array.map((textColour, number) => {
+    const keyB = `${textColour}${number}`;
+    const autoColour = textColour === backgroundColour;
+    const textColourMod = autoColour ? autoTextColourFromHex(backgroundColour) : textColour;
+    const contrastRatio = Number(getContrastRatioFromHex(backgroundColour, textColourMod).toFixed(2));
+    const contrastRating = contrast.makeContrastRating(contrastRatio);
+
+    return (
+      <ColourBlock
+        key={`${keyA}-${keyB}`}
+        backgroundColour={backgroundColour}
+        textColour={textColourMod}
+        clicked={clicked}
+        setClicked={setClicked}
+        autoColour={autoColour}
+        contrastRating={contrastRating}
+        contrastRatio={contrastRatio}
+      />
+    );
+  });
+  return { keyA, rowArray };
+}
+
 function createColourBlockArrays(
   coloursArray: Array<string>,
   clicked: boolean,
   setClicked: Dispatch<SetStateAction<boolean>>,
 ) {
   return coloursArray.map((backgroundColour, index, array) => {
-    const keyA = `${backgroundColour}${index}`;
-    const rowArray = array.map((textColour, number) => {
-      const keyB = `${textColour}${number}`;
-      const autoColour = textColour === backgroundColour;
-      const textColourMod = autoColour ? autoTextColourFromHex(backgroundColour) : textColour;
-      const contrastRatio = Number(getContrastRatioFromHex(backgroundColour, textColourMod).toFixed(2));
-      const contrastRating = contrast.makeContrastRating(contrastRatio);
-
-      return (
-        <ColourBlock
-          key={`${keyA}-${keyB}`}
-          backgroundColour={backgroundColour}
-          textColour={textColourMod}
-          clicked={clicked}
-          setClicked={setClicked}
-          autoColour={autoColour}
-          contrastRating={contrastRating}
-          contrastRatio={contrastRatio}
-        />
-      );
-    });
+    const { keyA, rowArray } = getBlockRow(backgroundColour, index, array, clicked, setClicked);
     return (
       <div
         key={`${backgroundColour}-${keyA}`}
