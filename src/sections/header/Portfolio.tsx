1a7428c480c94de05a9d8fc72f07beb568858eb3
GLD5000 <garethlouisdevlin@gmail.com>
2023-02-22 11:21:12 +0000

    Updated bg to match header

--- src/sections/header/Portfolio.tsx
+++ src/sections/header/Portfolio.tsx
@@ -1,6 +1,6 @@
 export default function Portfolio() {
   return (
-    <div className=" bg-neutral-100 dark:bg-neutral-700">
+    <div className=" bg-neutral-100 dark:bg-neutral-800">
       <h1 className=" m-6 whitespace-pre text-blue-800 dark:text-blue-400">Welcome the GLD portfolio!</h1>
       <button type="button" className="flex w-screen flex-col ">
         <h2 className="m-4 whitespace-pre text-blue-800 dark:text-blue-400">Click here to see more projects...</h2>
