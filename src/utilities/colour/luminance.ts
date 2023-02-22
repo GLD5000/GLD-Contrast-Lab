8c38317235f0c59210d8506d62b75634bbbe46c2
GLD5000 <garethlouisdevlin@gmail.com>
2023-02-22 09:26:08 +0000

    Extracted tests

--- src/utilities/colour/luminance.ts
+++ src/utilities/colour/luminance.ts
@@ -1,6 +1,6 @@
 import hexToSrgb from './colourSpace';
 
-const luminance = {
+export const luminance = {
   modifyColourValue(value: number) {
     return value <= 0.04045 ? value / 12.92 : ((value + 0.055) / 1.055) ** 2.4;
   },
@@ -25,40 +25,4 @@ const luminance = {
 
 export default function hexToLuminance(hex: string) {
   return luminance.convertHexToLuminance(hex);
-}
-
-if (import.meta.vitest) {
-  const { describe, expect, it } = import.meta.vitest;
-
-  (() => {
-    const testFunc = 'convertSrgbToLuminance';
-    const input = [1, 1, 1];
-    const output = 1;
-    describe(`#${testFunc}`, () => {
-      it(`Works for ${input}`, () => {
-        expect(luminance[testFunc](input)).toStrictEqual(output);
-      });
-    });
-  })();
-
-  (() => {
-    const testFunc = 'convertSrgbToLuminance';
-    const input = [0, 0, 0];
-    const output = 0;
-    describe(`#${testFunc}`, () => {
-      it(`Works for ${input}`, () => {
-        expect(luminance[testFunc](input)).toStrictEqual(output);
-      });
-    });
-  })();
-
-  (() => {
-    const input = '#fff';
-    const output = 1;
-    describe(`#hexToLuminance`, () => {
-      it(`Works for ${input}`, () => {
-        expect(hexToLuminance(input)).toStrictEqual(output);
-      });
-    });
-  })();
 }
