1cb56c05bed4e1e44c74e976fc41d1ba9d975621
GLD5000 <garethlouisdevlin@gmail.com>
2023-03-04 09:31:50 +0000

    Updated comboBox layout

--- src/sections/body/ComboBox.tsx
+++ src/sections/body/ComboBox.tsx
@@ -18,13 +18,13 @@ export default function ComboBox() {
       <label htmlFor="code-input">
         Colours <Span className="text-neutral-500" content="(separate with spaces or linebreaks)" />
       </label>
-      <div className="flex flex-row flex-wrap gap-2">
+      <div className="flex flex-row flex-wrap gap-2 bg-neutral-900">
         {list}
         <TextArea
           id="code-input"
           placeholder="Copy or write text in here..."
           name="codeInput"
-          className=" h-60 w-80 shrink-0 grow resize-y overflow-auto rounded bg-inherit text-inherit placeholder:text-gray-300"
+          className="shrink-0 grow resize-y overflow-auto rounded bg-inherit text-inherit placeholder:text-gray-300"
           value={textInput}
           onInput={(e: FormEvent<HTMLTextAreaElement>): void => {
             const { value: targetValue } = e.currentTarget;
