14e94964a249d16f4519402559f67e841604584e
GLD5000 <garethlouisdevlin@gmail.com>
2023-03-08 15:50:57 +0000

    Added infoTable column set fix #22

--- src/sections/body/InfoTable.tsx
+++ src/sections/body/InfoTable.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from 'react';
+import { ReactElement, useState } from 'react';
 import { useColourInputContext } from '../../contexts/ColourInputProvider';
 import { autoTextColour } from '../../utilities/colour/autoTextColour';
 import { colourSpace } from '../../utilities/colour/colourSpace';
@@ -16,89 +16,57 @@ function tableReducer(
   acc: {
     csv: string;
     jsx: ReactElement[][];
+    dataColumns: Set<string>;
   },
   curr: string,
 ): {
   csv: string;
   jsx: ReactElement[][];
+  dataColumns: Set<string>;
 } {
   const classNames = 'block w-40 p-4 text-xs rounded-none';
   const luminanceFloat = luminance.convertHexToLuminance(curr);
-  const HSLValue = colourSpace.convertHexToHslString(curr);
-  const RGBValue = colourSpace.convertHextoRgbString(curr);
-  const LuminanceValue = luminance.convertHexToLuminancePercent(curr);
-  const ContrastRatioBlackValue = contrast.getContrastRatio2Dp([0, luminanceFloat]);
-  const ContrastRatioWhiteValue = contrast.getContrastRatio2Dp([1, luminanceFloat]);
 
-  const newCsvRow = `\r\n${curr}\t${HSLValue}\t${RGBValue}\t${LuminanceValue}\t${ContrastRatioBlackValue}\t${ContrastRatioWhiteValue}`;
-  acc.csv += newCsvRow;
-  const currJsx = (
-    <span key={`${curr}-currP`} className={`${classNames}`}>
-      {curr}
+  const valuesObject: { [elemName: string]: string } = {
+    Hex: curr,
+    HSL: colourSpace.convertHexToHslString(curr),
+    RGB: colourSpace.convertHextoRgbString(curr),
+    Luminance: luminance.convertHexToLuminancePercent(curr),
+    Black: `${contrast.getContrastRatio2Dp([0, luminanceFloat])}`,
+    White: `${contrast.getContrastRatio2Dp([1, luminanceFloat])}`,
+  };
+
+  const newerRow = Object.values(valuesObject).reduce(titleRowReducer, '\r\n');
+  acc.csv += newerRow;
+
+  const newJsxRow = [...acc.dataColumns].map((key) => (
+    <span key={`${curr}-${key}`} className={`${classNames}`}>
+      {valuesObject[key]}
     </span>
-  );
-  const HSLJsx = (
-    <span key={`${curr}-HSL`} className={`${classNames}`}>
-      {HSLValue}
-    </span>
-  );
-  const RGBJsx = (
-    <span key={`${curr}-RGB`} className={`${classNames}`}>
-      {RGBValue}
-    </span>
-  );
-  const LuminanceJsx = (
-    <span key={`${curr}-Luminance`} className={`${classNames}`}>
-      {LuminanceValue}
-    </span>
-  );
-  const ContrastRatioBlackJsx = (
-    <span key={`${curr}-ContrastRatioBlack`} className={`${classNames}`}>
-      {ContrastRatioBlackValue}
-    </span>
-  );
-  const ContrastRatioWhiteJsx = (
-    <span key={`${curr}-ContrastRatioWhite`} className={`${classNames}`}>
-      {ContrastRatioWhiteValue}
-    </span>
-  );
-  const newRow: Array<ReactElement> = [
-    currJsx,
-    HSLJsx,
-    RGBJsx,
-    LuminanceJsx,
-    ContrastRatioBlackJsx,
-    ContrastRatioWhiteJsx,
-  ];
+  ));
+  const newRow: Array<ReactElement> = newJsxRow;
   acc.jsx.push(newRow);
   return acc;
 }
-function getTable(colourArray: string[]) {
+function titleRowReducer(previousValue: string, currentValue: string, currentIndex: number, array: string[]) {
+  const returnValue = previousValue + (currentIndex < array.length - 1 ? `${currentValue}\t` : `${currentValue}`);
+  return returnValue;
+}
+
+function getTable(colourArray: string[], dataColumns: Set<string>) {
   const classNames = ' block w-40 p-2 text-sm rounded-none text-center my-auto';
+  const jsxArray = [...dataColumns].map((key) => {
+    const content = key === 'White' || key === 'Black' ? `Contrast ${key}` : key;
+    return (
+      <b key={key} id={`${key}-table`} className={`${classNames}`}>
+        {content}
+      </b>
+    );
+  });
   const tableAccumulator = {
     csv: 'Hex\tHSL\tRGB\tLuminance\tBlack\tWhite',
-    jsx: [
-      [
-        <b key="Hex" className={`${classNames}`}>
-          Hex
-        </b>,
-        <b key="HSL" className={`${classNames}`}>
-          HSL
-        </b>,
-        <b key="RGB" className={`${classNames}`}>
-          RGB
-        </b>,
-        <b key="Luminance" className={`${classNames}`}>
-          Luminance
-        </b>,
-        <b key="Black" className={`${classNames}`}>
-          Contrast Black
-        </b>,
-        <b key="White" className={`${classNames}`}>
-          Contrast White
-        </b>,
-      ],
-    ],
+    jsx: [[...jsxArray]],
+    dataColumns,
   };
   const { csv, jsx } = colourArray.reduce(tableReducer, tableAccumulator);
   const flexBoxes = jsx.map((x, i) => {
@@ -118,12 +86,14 @@ function getTable(colourArray: string[]) {
 
 export default function InfoTable() {
   const { colourSet } = useColourInputContext();
+  const [dataColumns, setDataColumns] = useState(new Set(['Hex', 'Luminance']));
+  console.log('setDataColumns:', setDataColumns);
   if (colourSet.size === 0) return null;
   const lumSort = [...colourSet].reduce(sortByLuminance, []).flatMap((x) => x);
 
-  const tableMarkDown = getTable(lumSort);
+  const tableMarkDown = getTable(lumSort, dataColumns);
   return (
-    <div className="relative w-full overflow-x-auto px-8 pb-4">
+    <div className="relative w-full overflow-x-auto pb-4">
       <div className="relative mx-auto flex w-fit grow  flex-col gap-0 overflow-clip rounded border border-neutral-900 bg-white text-center text-neutral-800 dark:border-neutral-300 dark:bg-neutral-700 dark:text-neutral-50">
         {tableMarkDown}
       </div>
