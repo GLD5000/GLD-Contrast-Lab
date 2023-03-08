a5a266d72ec9b4af388a711106813a3ac4dc41d5
GLD5000 <garethlouisdevlin@gmail.com>
2023-03-08 11:52:58 +0000

    Removed fill

--- src/icons/DuplicateSvg.tsx
+++ src/icons/DuplicateSvg.tsx
@@ -1,10 +1,14 @@
-export default function DuplicateSvg({ classes = 'stroke-1 fill-neutral-700 stroke-current' }) {
+import { CSSProperties } from 'react';
+
+export default function DuplicateSvg({ classes = 'stroke-1 fill-none stroke-current' }) {
+  const styles: CSSProperties | undefined = { strokeLinecap: 'round', strokeLinejoin: 'round' };
   return (
     <div className="pointer-events-none h-full w-full">
       <svg id="duplicate-svg" role="img" aria-label="copy" height="100%" width="100%" viewBox="0 0 16 16">
-        <rect x="1" y="1" width="10" height="10" rx="2" className={classes} />
-        <rect x="5" y="5" width="10" height="10" rx="2" className={classes} />
+        <path d="M 11,2 H 2 v 9" className={classes} style={styles} />
+        <rect x="4" y="4" width="9" height="9" className={classes} style={styles} />
       </svg>
     </div>
   );
 }
+// rx="1"
