aa01294c322eddcbdc9da1d699400fcf9a45fb50
GLD5000 <garethlouisdevlin@gmail.com>
2023-03-11 22:49:56 +0000

    Add footer fix #35

--- src/icons/GldSvg.tsx
+++ src/icons/GldSvg.tsx
@@ -1,7 +1,7 @@
-export default function GldSvg() {
+export default function GldSvg({ wrapperClasses = ' pointer-events-none h-12 w-12 fill-current' }) {
   return (
-    <div className=" pointer-events-none h-12 w-12 fill-current">
-      <svg id="gld-svg" className="gld-svg" role="img" aria-label="GLD" height="100%" width="100%" viewBox="0 0 16 16">
+    <div className={wrapperClasses}>
+      <svg id="gld-svg" role="img" aria-label="GLD" height="100%" width="100%" viewBox="0 0 16 16">
         <g>
           <path d="M 6,2 H 2 V 3 H 1 V 10 H 2 V 11 H 6 V 6 H 4 v 2 h 1 v 1 H 3 V 4 H 4 v 1 h 2 z" id="path510" />
           <path d="M 10,14 H 7 l -1e-7,-10 H 9 l 10e-8,8 H 10 Z" id="path512" />
