703aebeb717e94e9cf2464a9f574dc4d276de398
GLD5000 <garethlouisdevlin@gmail.com>
2023-03-11 19:08:34 +0000

    Add portfolio link to logo  fix #34

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -14,7 +14,11 @@ export default function Header({
     <header className="sticky top-0 left-0 right-0 z-[999] grid h-fit w-screen flex-shrink-0 flex-grow-0 grid-cols-frAutoFr content-center bg-neutral-100 dark:bg-neutral-800">
       <nav className=" col-start-2 flex w-body min-w-body max-w-body flex-wrap items-center justify-between  ">
         <div className="flex h-16 flex-wrap items-center gap-4 p-2">
-          <GldSvg />
+          <a href="https://gld-portfolio.vercel.app/" target="_blank" rel="noreferrer">
+            {' '}
+            <GldSvg />
+          </a>
+
           <h2>{title}</h2>
         </div>
         <div className="relative flex h-16 flex-wrap items-center justify-center gap-4 py-2">
