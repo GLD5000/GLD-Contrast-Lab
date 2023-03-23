be351c5deac357b886d4f68c7a2df7056f06c3d2
GLD5000 <garethlouisdevlin@gmail.com>
2023-03-23 09:19:16 +0000

    Updated padding sm:

--- src/sections/Body.tsx
+++ src/sections/Body.tsx
@@ -12,10 +12,12 @@ export default function Body() {
           <div className=" w-body-sm min-w-body max-w-body grow sm:w-body">
             <div className="mx-auto grid w-screen max-w-body items-center self-center px-4 py-10 sm:w-full sm:px-8">
               <div className=" mx-auto mt-10 flex flex-row overflow-clip rounded-lg border-2 border-current ">
-                <h1 className=" rounded-none bg-white p-3 font-black  leading-6 text-black sm:p-4 sm:text-6xl">
+                <h1 className=" rounded-none bg-white p-3 font-black  leading-6 text-black sm:p-[0.95rem] sm:text-6xl">
                   CONTRAST
                 </h1>
-                <h1 className="  rounded-none bg-black p-3 font-black leading-6 text-white sm:p-4 sm:text-6xl">TOOL</h1>
+                <h1 className="  rounded-none bg-black p-3 font-black leading-6 text-white sm:p-[0.95rem] sm:text-6xl">
+                  TOOL
+                </h1>
               </div>
               <b className="mx-auto mt-2 mb-20 text-center text-xl">Create Accessible Colour Combinations</b>
 
