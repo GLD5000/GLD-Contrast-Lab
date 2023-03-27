0fd7571c78905bb1e8b5ad3d572d94ae779886e0
GLD5000 <garethlouisdevlin@gmail.com>
2023-03-27 17:32:34 +0100

    Added hero section

---
+++ src/sections/body/HeroSection.tsx
@@ -0,0 +1,20 @@
+export default function HeroSection() {
+  return (
+    <div className="mx-auto grid">
+      <div className=" mx-auto mt-24 flex flex-row overflow-clip rounded-lg border-2 border-current ">
+        <h1 className=" rounded-none bg-white p-3 font-black  leading-6 text-black sm:p-[0.95rem] sm:text-6xl">
+          CONTRAST
+        </h1>
+        <h1 className="  rounded-none bg-black p-3 font-black leading-6 text-white sm:p-[0.95rem] sm:text-6xl">TOOL</h1>
+      </div>
+      <b className="mx-auto mt-2 mb-20 w-fit text-center text-xl">Create Accessible Colour Combinations</b>
+      <p>
+        Quickly compare any number of colours and ensure that they meet current standards for visual Contrast Ratios and
+        accessibility (WCAG 2.1).
+      </p>
+      <a className=" underline" href="https://glddevtips.web.app/?title=WCAG%202.1%20Contrast%20Guidance">
+        Read a brief summary of the Contrast Ratio Guidance on GLD DevTips
+      </a>
+    </div>
+  );
+}
