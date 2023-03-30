9837afaffa7f10d7cb1742443a8e9fcd626746a0
GLD5000 <garethlouisdevlin@gmail.com>
2023-03-30 17:43:47 +0100

    Updated hamburger icon visibility

--- src/sections/header/NavBar.tsx
+++ src/sections/header/NavBar.tsx
@@ -77,7 +77,7 @@ export default function NavBar({
           id="colour-theme-button-main"
           name="Dark Mode Button"
           className="rounded text-xs"
-          buttonClasses=" text-txt-mid hover:text-txt-main hover:underline hover:decoration-current hover:underline-offset-2 hover:transition focus:text-txt-main focus:underline focus:decoration-current  focus:underline-offset-2 focus:transition dark:text-txt-mid-dk dark:hover:text-txt-main-dk dark:focus:text-txt-main-dk "
+          buttonClasses=" sm:hidden text-txt-mid hover:text-txt-main hover:underline hover:decoration-current hover:underline-offset-2 hover:transition focus:text-txt-main focus:underline focus:decoration-current focus:underline-offset-2 focus:transition dark:text-txt-mid-dk dark:hover:text-txt-main-dk dark:focus:text-txt-main-dk "
           textElement={null}
           svg={<HamburgerSvg />}
         />
