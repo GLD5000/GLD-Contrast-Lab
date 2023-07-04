5d0b5d4f40b4517e8d5160bb61b0f766a9b452d7
GLD5000 <garethlouisdevlin@gmail.com>
2023-07-04 10:30:24 +0100

    Update comment

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,10 +2,10 @@ module.exports = {
   // this will check Typescript files
   '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
 
-  // This will lint and format TypeScript and                                             //JavaScript files
+  // This will format and lint TS & JS Files
   '**/*.(ts|tsx|js)': (filenames) => [
-    `yarn eslint --fix ${filenames.join(' ')}`,
     `yarn prettier --write ${filenames.join(' ')}`,
+    `yarn eslint --fix ${filenames.join(' ')}`,
   ],
 
   // this will Format MarkDown and JSON
